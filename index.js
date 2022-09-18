e74329c674a0826300fcbc7e4eb2ec8b17baefe6
Belhadj Mohamed <rafikgamer8@gmail.com>
2022-09-18 22:45:05 +0200

    first commit

--- index.js
+++ index.js
@@ -1,29 +1,151 @@
+// setup
+ballLimit = 200;
+let lightSpeedVelocityBoost = 0.0001;
+
+// canvas setup
 const canvas = document.getElementById("canvas");
 const c = canvas.getContext("2d"); // create a context
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
-const ballLimit = 100;
-let lightSpeed = false;
-let lightSpeedVelocityBoost = 0.01;
-const mousePosition = {
-  x: 9999999,
-  y: 9999999,
+let startAnimation = false;
+// audio setup
+let averageBars = 0;
+const audioElement = document.getElementById("audio");
+const source = "./assets/zedd.mp3";
+const audio = new Audio(source);
+
+let analyser, audioSourse, bufferLength, dataArray, audioCtx;
+
+const drawArc = (up) => {
+  c.beginPath();
+  const shake = !startAnimation ? 0 : random(-1, 1);
+  if (up) {
+    c.arc(
+      innerWidth / 2,
+      innerHeight / 2 + shake,
+      140,
+      degrees_to_radians(200),
+      degrees_to_radians(-20),
+      false
+    );
+  } else {
+    c.arc(
+      innerWidth / 2,
+      innerHeight / 2 + shake,
+      140,
+      degrees_to_radians(20),
+      degrees_to_radians(160),
+      false
+    );
+  }
+  c.lineWidth = 3.5;
+  addGlow("white");
+  c.strokeStyle = "white";
+  c.stroke();
+  c.closePath();
+  // the four lines
+  if (up) {
+    // upper circle
+    lastX = innerWidth / 2 + 140 * Math.cos(degrees_to_radians(-20));
+    lastY = innerHeight / 2 + 140 * Math.sin(degrees_to_radians(-20));
+    // right
+    c.beginPath();
+    c.moveTo(lastX, lastY + shake);
+    c.lineTo(lastX + 50, lastY + shake);
+    addGlow("white");
+    c.stroke();
+    // left
+    c.beginPath();
+    c.moveTo(lastX - 131 * 2, lastY + shake);
+    c.lineTo(lastX - 131 * 2 - 50, lastY + shake);
+    addGlow("white");
+    c.stroke();
+  } else {
+    // upper circle
+    lastX = innerWidth / 2 + 140 * Math.cos(degrees_to_radians(160));
+    lastY = innerHeight / 2 + 140 * Math.sin(degrees_to_radians(160));
+    // right
+    c.beginPath();
+    c.moveTo(lastX, lastY);
+    c.lineTo(lastX - 50, lastY);
+    addGlow("white");
+    c.stroke();
+    // left
+    c.beginPath();
+    c.moveTo(lastX + 131 * 2, lastY);
+    c.lineTo(lastX + 131 * 2 + 50, lastY);
+    addGlow("white");
+    c.stroke();
+  }
 };
 
-document.addEventListener("mousemove", (e) => {
-  mousePosition.x = e.x;
-  mousePosition.y = e.y;
-  return;
-});
-document.onmousedown = () => {
-  lightSpeed = true;
+const initBars = () => {
+  audioSourse = audioCtx.createMediaElementSource(audio);
+  analyser = audioCtx.createAnalyser();
+  audioSourse.connect(analyser);
+  analyser.connect(audioCtx.destination);
+  analyser.fftSize = 128;
+  bufferLength = analyser.frequencyBinCount;
+  dataArray = new Uint8Array(bufferLength);
 };
-document.onmouseup = () => {
-  lightSpeed = false;
+
+canvas.onclick = () => {
+  audioCtx = new AudioContext();
+  initBars();
+  startAnimation = !startAnimation;
+  audio.play();
+};
+
+const drawCol = ({ barHeight, barWidth, degree, secondary }) => {
+  const levels = Math.round(barHeight / 10);
+  c.save();
+  c.translate(innerWidth / 2, innerHeight / 2);
+  c.rotate(degrees_to_radians(degree));
+  for (var i = 0; i < levels; i++) {
+    addGlow("white");
+    c.fillStyle = hex2rgba(secondary, i * 0.1);
+    c.fillRect(0, 10 * i + 10, barWidth, 10);
+  }
+  c.restore();
+};
+
+const animateBars = (secondary) => {
+  // circles width / the amount of bars
+  const barWidth = (2 * Math.PI * 100) / bufferLength;
+  let barHeight;
+  let degree = 0;
+  // array of values between 0 and 255
+  analyser.getByteFrequencyData(dataArray);
+  averageBars =
+    dataArray.reduce((prev, current) => prev + current, 0) + dataArray.length;
+  for (let i = 0; i < bufferLength; i++) {
+    barHeight = dataArray[i] / 2 > 100 ? random(90, 100) : dataArray[i] / 2;
+    drawCol({ barHeight, barWidth, degree, secondary });
+    degree += 30 + i * 2;
+  }
+};
+// the lines on the left and right side of the circle
+const drawGuides = ({ start, end, addToInner }) => {
+  if (addToInner) {
+    c.save();
+    c.translate(innerWidth / 2, innerHeight / 2);
+    c.rotate(degrees_to_radians(270));
+    addGlow("white");
+    c.fillStyle = "#fff";
+    c.fillRect(0, start, 5, end);
+    c.restore();
+  } else {
+    c.save();
+    c.translate(innerWidth / 2, innerHeight / 2);
+    c.rotate(degrees_to_radians(90));
+    addGlow("white");
+    c.fillStyle = "#fff";
+    c.fillRect(0, start, 5, end);
+    c.restore();
+  }
 };
 
 // utility functions
-
 function random(min, max) {
   /**
    * Returns a random integer between min (inclusive) and max (inclusive).
@@ -33,6 +155,10 @@ function random(min, max) {
    * Using Math.round() will give you a non-uniform distribution!
    */
   return Math.random() * (max - min) + min;
+}
+// degrees to rad
+function degrees_to_radians(degrees) {
+  return degrees * (Math.PI / 180);
 }
 // find the distance between two points or two circles
 function diff({ xv, yv, radius = 0 }, { xv1, yv1, radius1 = 0 }) {
@@ -50,11 +176,88 @@ function diff({ xv, yv, radius = 0 }, { xv1, yv1, radius1 = 0 }) {
   const distance = Math.sqrt(Math.pow(xDiff, 2) + Math.pow(yDiff, 2));
   return distance - (radius1 + radius);
 }
+const addGlow = (color) => {
+  c.shadowColor = color;
+  c.shadowBlur = 25;
+  c.shadowOffsetX = 1;
+  c.shadowOffsetY = 1;
+};
+
 // hex2rgba
 const hex2rgba = (hex, alpha = 1) => {
   const [r, g, b] = hex.match(/\w\w/g).map((x) => parseInt(x, 16));
   return `rgba(${r},${g},${b},${alpha})`;
 };
+// text
+class Zedd {
+  constructor({ primaryColor = "#00cccc", secondaryColor = "#be2ed6" }) {
+    this.primary = primaryColor;
+    this.secondary = secondaryColor;
+    this.alpha = 0;
+    this.fadeSpeed = 0.005;
+    this.rotationSpeed = 0.3;
+    this.currentDegree = 0;
+    this.degree = 0;
+  }
+  drawZedd() {
+    // draw circle around the z
+    c.beginPath();
+    c.save();
+    addGlow("black");
+    c.arc(
+      innerWidth / 2, //x
+      innerHeight / 2, //y
+      averageBars == 0 ? 120 : 110 + averageBars / 100, //radius
+      0, //start angle
+      Math.PI * 2, //end angle
+      false
+    );
+    const gradient = c.createRadialGradient(
+      innerWidth / 2,
+      innerHeight / 2,
+      50, //radius of the inner circle
+      innerWidth / 2,
+      innerHeight / 2,
+      120 //radius of the outer cirlce
+    );
+    gradient.addColorStop(0, "#00cccc");
+    gradient.addColorStop(
+      averageBars == 0 ? 0.9 : 0.9 - Math.abs(averageBars / 10000),
+      "#000"
+    );
+    c.fillStyle = gradient;
+    c.fill();
+    c.closePath();
+    c.restore();
+    // if the animation started draw some bars
+
+    // draw z
+    c.save();
+    c.translate(innerWidth / 2, innerHeight / 2);
+    if (startAnimation) {
+      c.rotate(degrees_to_radians(this.degree));
+      this.degree += this.rotationSpeed;
+    }
+    c.font = `${
+      averageBars == 150 ? 150 : 150 + Math.abs(averageBars / 100)
+    }px Ultra`;
+    c.fillStyle = hex2rgba("#000000", this.alpha);
+    c.rotate(degrees_to_radians(this.currentDegree));
+    c.textAlign = "center";
+    c.textBaseline = "middle";
+    c.fillText("z", 0, 0, innerHeight / 2);
+    c.restore();
+
+    // if the text is not fully visible add fade speed to the alpha
+    if (this.alpha < 1) {
+      this.alpha += this.fadeSpeed;
+    }
+  }
+}
+
+const zedd1 = new Zedd({});
+
+////////////galaxy
 
 // random colors
 var colors = [
@@ -127,7 +330,7 @@ class Ball {
     this.radius = radius;
     this.color = color;
     this.velocity = velocity;
-    this.steem = 0;
+    this.steem = 0.1;
     this.rad = Math.random() * Math.PI * 2;
   }
   update() {
@@ -137,31 +340,25 @@ class Ball {
     this.draw();
   }
   draw() {
-    if (this.steem > 0 || lightSpeed) {
-      // glow
-      c.beginPath();
-      c.arc(this.xv, this.yv, this.radius * 5, Math.PI * 2, false);
-      c.fillStyle = hex2rgba(this.color, this.steem);
-      c.fill();
-      c.closePath();
-      // if the lightspeed is on and the steem tank isn't full start filling it up
-      if (this.steem < 0.06 && lightSpeed) {
-        this.steem += 0.001;
-        if (this.velocity <= 0.009) {
-          this.velocity += lightSpeedVelocityBoost;
-        }
-      }
-      // if the lightspeed is off and there is steem in the tank use it
-      else if (!lightSpeed) {
-        this.steem -= 0.0005;
-        if (this.steem < 0.009) {
-          this.steem = 0;
-        }
-        if (this.velocity > 0.009) {
-          this.velocity -= lightSpeedVelocityBoost;
-        }
+    c.beginPath();
+    c.arc(this.xv, this.yv, this.radius * 5, Math.PI * 2, false);
+    c.fillStyle = hex2rgba(this.color, this.steem);
+    c.fill();
+    c.closePath();
+
+    // if the startAnimation is on and the steem tank isn't full start filling it up
+    if (startAnimation) {
+      if (this.velocity <= 0.009) {
+        this.velocity += lightSpeedVelocityBoost;
       }
     }
+    // if the startAnimation is off and there is steem in the tank use it
+    else if (!startAnimation) {
+      if (this.velocity > 0.009) {
+        this.velocity -= lightSpeedVelocityBoost;
+      }
+    }
+
     // circle
     c.beginPath();
     c.arc(this.xv, this.yv, this.radius, Math.PI * 2, false);
@@ -175,7 +372,7 @@ for (var i = 0; i < ballLimit; i++) {
   const config = {
     width: this.height,
     color: colors[Math.round(random(0, 16))],
-    velocity: 0.009,
+    velocity: 0.0009,
     radius: random(0.5, 1),
     xv: innerWidth / 2,
     yv: innerHeight / 2,
@@ -184,18 +381,36 @@ for (var i = 0; i < ballLimit; i++) {
   ballsArray.push(new Ball(config));
 }
 // animate ball1
-function animate() {
-  if (lightSpeed) {
-    c.fillStyle = "rgba(0, 0, 0, 0.1)";
-  } else {
-    c.fillStyle = "rgba(0, 0, 0, 1)";
-  }
-  c.fillRect(0, 0, innerWidth, innerHeight);
+function animateGalaxy() {
   if (ballsArray.length > 0) {
     for (var i = 0; i < ballsArray.length; i++) {
       ballsArray[i].update();
     }
   }
-  requestAnimationFrame(animate);
 }
+
+const animationRoot = () => {
+  animateGalaxy();
+  drawArc(true);
+  drawArc();
+
+  zedd1.drawZedd();
+
+  if (startAnimation) {
+    animateBars("#ffffff");
+    const randomValue = random(100, 200);
+    drawGuides({ start: 140, end: randomValue, addToInner: true });
+    drawGuides({ start: 140, end: randomValue, addToInner: false });
+  } else {
+    drawGuides({ start: 140, end: 150, addToInner: true });
+    drawGuides({ start: 140, end: 150, addToInner: false });
+  }
+};
+const animate = () => {
+  c.clearRect(0, 0, innerWidth, innerHeight);
+  // start the animation
+  animationRoot();
+
+  requestAnimationFrame(animate);
+};
 animate();
